6419655bbf98eda5832bfdf0aa856424b706963a
Juan Carlos <juanc.bricenof@gmail.com>
2024-03-01 10:22:32 -0300

    Se modifica pom y se agrega metodo para capturar pantalla

--- src/test/java/EjemploSimpleSelenium.java
+++ src/test/java/EjemploSimpleSelenium.java
@@ -1,7 +1,10 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
 import java.lang.InterruptedException;
 import static java.lang.Thread.sleep;
 
@@ -14,11 +17,17 @@ public class EjemploSimpleSelenium {
             //Instanciar el controlador de Chrome
             WebDriver driver = new ChromeDriver();
 
+            //Ruta donde se almacena las capturas de pantalla
+            String screenshotDir = "D:\\MiPrimerScripWeb\\src\\test\\resources\\evidencias";
+
             //Abre url en el navegador
             driver.get("https://linio.falabella.com/linio-cl");
 
             //Maximizar el navegador
             driver.manage().window().maximize();
+
+            //Realiza acciones y toma captura de pantalla
+            takeScreenshot(driver, screenshotDir, "screenshot-1.png");
 
             //Espera un momento para que puedas ver que se ha abierto el navegador
 
@@ -39,17 +48,46 @@ public class EjemploSimpleSelenium {
             sleep(5000);
 
             //Paso 2 escribir el texto a buscar
-            txtBuscar.sendKeys("Libro de Selenium en español");
+            txtBuscar.sendKeys("laptop gamer");
+            takeScreenshot(driver, screenshotDir, "screenshot-2.png");
             sleep(5000);
 
             //Paso 3 hacer click en el botón de búsqueda (imagen de lupa)
             btnBuscar.click();
             sleep(5000);
 
+            //Buscar un texto y almacenar el valor de la página web (resultados)
+            WebElement lblResultado = driver.findElement(By.id("testId-SearchLandingContainer-totalResults"));
+            String resultado = lblResultado.getText();
+            String resultadoEsperado = "Resultados (292)";
+
+            //Validación de resultado esperado vs resultado encontrado
+            Assertions.assertEquals(resultadoEsperado, resultado);
+            takeScreenshot(driver, screenshotDir, "screenshot-3.png");
+            System.out.println(resultado);
+            sleep(5000);
+
             //Cerrar el navegador
             sleep(5000);
             driver.quit();
 
+        }
+
+        public static void takeScreenshot(WebDriver driver, String screenshotDir, String fileName) {
+            //Toma la captura de pantalla de la página web
+            File screenshotFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            //Define la ruta de almacenamiento de la imagen
+            String destinationPath = screenshotDir + fileName;
+
+            //Copiar el archivo a la ruta de almacenamiento
+            try {
+                FileUtils.copyFile(screenshotFile, new  File(destinationPath));
+                System.out.println("Captura de la imagen exitosa");
+            } catch (IOException e) {
+                System.out.println("Ha ocurrido un error al copiar la imagen");
+                throw new RuntimeException(e);
+
+            }
         }
 
     }
